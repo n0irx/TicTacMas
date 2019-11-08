b27265721c55cecf74bef0251c8c331fd8b479bc
Roshani Ayu Pranasti <asti.roshani@gmail.com>
2019-11-08 12:59:33 +0700

    accepted current changes

--- js/4x4.js
+++ js/4x4.js
@@ -25,11 +25,7 @@ timunMasImg.src = timunMasImgPath;
 butoIjoImg.src = butoIjoImgPath;
 
 function newboard() {
-<<<<<<< HEAD
     for(let i = 0; i < BOARD_SIZE; i++) {
-=======
-    for (let i = 0; i < BOARD_SIZE; i++) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
         board[i] = NOT_OCCUPIED;
         document.images[i].src = blank_src;
     }
@@ -40,17 +36,10 @@ function newboard() {
 }
 
 function makeMove(pieceMove) {
-<<<<<<< HEAD
     if(!isGameOver(board) && board[pieceMove] === NOT_OCCUPIED) {
         board[pieceMove] = TIMUN_MAS;
         document.images[pieceMove].src = timunMasImgPath;
         if(!isGameOver(board)) {
-=======
-    if (!isGameOver(board) && board[pieceMove] === NOT_OCCUPIED) {
-        board[pieceMove] = TIMUN_MAS;
-        document.images[pieceMove].src = timunMasImgPath;
-        if (!isGameOver(board)) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
             var alert = document.getElementById("turnInfo");
             active_turn = "BUTO_IJO";
             alert.innerHTML = "Bagian Buto ijo Menyerang"
@@ -66,11 +55,7 @@ function moveButoIjo() {
     document.images[move].src = butoIjoImgPath;
     choice = [];
     active_turn = "TIMUN_MAS"
-<<<<<<< HEAD
     if(!isGameOver(board)) {
-=======
-    if (!isGameOver(board)) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
         var alert = document.getElementById("turnInfo");
         alert.innerHTML = "Timun mas, pikirkan strategi baik untuk kabur";
     }
@@ -78,11 +63,7 @@ function moveButoIjo() {
 
 function gameScore(currentBoard, depth) {
     var score = checkWinningCondition(currentBoard);
-<<<<<<< HEAD
     if(score === 1) {
-=======
-    if (score === 1) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
         return 0;
     } else if (score === 2) {
         return depth - 10;
@@ -92,16 +73,10 @@ function gameScore(currentBoard, depth) {
 }
 
 function minimax(node, depth, alpha, beta) {
-<<<<<<< HEAD
     if(checkWinningCondition(node) === 1 ||
         checkWinningCondition(node) === 2 ||
         checkWinningCondition(node) === 3)
     {
-=======
-    if (checkWinningCondition(node) === 1 ||
-        checkWinningCondition(node) === 2 ||
-        checkWinningCondition(node) === 3) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
         return gameScore(node, depth);
     }
 
@@ -110,59 +85,33 @@ function minimax(node, depth, alpha, beta) {
 
     var availableMoves = getAvailableMoves(node);
     var move, result, possibleGameResult;
-<<<<<<< HEAD
     if(active_turn === "BUTO_IJO") {
         for(var i = 0; i < availableMoves.length; i++) {
-=======
-    if (active_turn === "BUTO_IJO") {
-        for (var i = 0; i < availableMoves.length; i++) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
             move = availableMoves[i];
             possibleGameResult = getNewState(move, node);
             result = minimax(possibleGameResult, depth, alpha, beta);
             node = undoMove(node, move);
-<<<<<<< HEAD
             if(result > alpha) {
                 alpha = result
                 if(depth === 1) {
                     choice = move
                 }
             } else if (alpha >= beta)  {
-=======
-            if (result > alpha) {
-                alpha = result
-                if (depth === 1) {
-                    choice = move
-                }
-            } else if (alpha >= beta) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
                 return alpha;
             }
         }
         return alpha;
     } else {
-<<<<<<< HEAD
         for(var i = 0; i < availableMoves.length; i++) {
-=======
-        for (var i = 0; i < availableMoves.length; i++) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
             move = availableMoves[i];
             possibleGameResult = getNewState(move, node);
             result = minimax(possibleGameResult, depth, alpha, beta);
             node = undoMove(node, move);
-<<<<<<< HEAD
             if(result < beta) {
                 beta = result
                 if(depth === 1) {
                     choice = move
                 }
-=======
-            if (result < beta) {
-                beta = result
-                if (depth === 1) {
-                    choice = move
-                }
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
             } else if (beta <= alpha) {
                 return beta;
             }
@@ -185,11 +134,7 @@ function getNewState(move, currentBoard) {
 
 function changeTurn() {
     var piece;
-<<<<<<< HEAD
     if(active_turn === "BUTO_IJO") {
-=======
-    if (active_turn === "BUTO_IJO") {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
         piece = 'X';
         active_turn = "TIMUN_MAS";
     } else {
@@ -201,13 +146,8 @@ function changeTurn() {
 
 function getAvailableMoves(currentBoard) {
     var possibleMoves = new Array();
-<<<<<<< HEAD
     for(var i=0; i < BOARD_SIZE; i++) {
         if(currentBoard[i] === NOT_OCCUPIED) {
-=======
-    for (var i = 0; i < BOARD_SIZE; i++) {
-        if (currentBoard[i] === NOT_OCCUPIED) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
             possibleMoves.push(i);
         }
     }
@@ -222,12 +162,8 @@ function getAvailableMoves(currentBoard) {
 function checkWinningCondition(currentBoard) {
 
     // checking for horizontal wins
-<<<<<<< HEAD
     for (i = 0; i <= 12; i += 4)
     {
-=======
-    for (i = 0; i <= 12; i += 4) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
         if (currentBoard[i] === TIMUN_MAS && currentBoard[i + 1] === TIMUN_MAS && currentBoard[i + 2] === TIMUN_MAS && currentBoard[i + 3] === TIMUN_MAS)
             return 2;
         if (currentBoard[i] === BUTO_IJO && currentBoard[i + 1] === BUTO_IJO && currentBoard[i + 2] === BUTO_IJO && currentBoard[i + 3] === BUTO_IJO)
@@ -235,12 +171,8 @@ function checkWinningCondition(currentBoard) {
     }
 
     // Check for vertical wins
-<<<<<<< HEAD
     for (i = 0; i <= 3; i++)
     {
-=======
-    for (i = 0; i <= 3; i++) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
         if (currentBoard[i] === TIMUN_MAS && currentBoard[i + 4] === TIMUN_MAS && currentBoard[i + 8] === TIMUN_MAS && currentBoard[i + 12] === TIMUN_MAS)
             return 2;
         if (currentBoard[i] === BUTO_IJO && currentBoard[i + 4] === BUTO_IJO && currentBoard[i + 8] === BUTO_IJO && currentBoard[i + 12] === BUTO_IJO)
@@ -249,7 +181,6 @@ function checkWinningCondition(currentBoard) {
 
     // Check for diagonal wins
     if ((currentBoard[0] === TIMUN_MAS && currentBoard[5] === TIMUN_MAS && currentBoard[10] === TIMUN_MAS && currentBoard[15] === TIMUN_MAS) ||
-<<<<<<< HEAD
             (currentBoard[3] === TIMUN_MAS && currentBoard[6] === TIMUN_MAS && currentBoard[9] === TIMUN_MAS && currentBoard[12] === TIMUN_MAS))
         return 2;
 
@@ -263,20 +194,6 @@ function checkWinningCondition(currentBoard) {
         if (currentBoard[i] !== TIMUN_MAS && currentBoard[i] !== BUTO_IJO)
             return 0;
     }
-=======
-        (currentBoard[3] === TIMUN_MAS && currentBoard[6] === TIMUN_MAS && currentBoard[9] === TIMUN_MAS && currentBoard[12] === TIMUN_MAS))
-        return 2;
-
-    if ((currentBoard[0] === BUTO_IJO && currentBoard[5] === BUTO_IJO && currentBoard[10] === BUTO_IJO && currentBoard[15] === BUTO_IJO) ||
-        (currentBoard[3] === BUTO_IJO && currentBoard[6] === BUTO_IJO && currentBoard[9] === BUTO_IJO && currentBoard[12] === BUTO_IJO))
-        return 3;
-
-    // Check for tie
-    for (i = 0; i < BOARD_SIZE; i++) {
-        if (currentBoard[i] !== TIMUN_MAS && currentBoard[i] !== BUTO_IJO)
-            return 0;
-    }
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
     return 1;
 }
 
@@ -287,21 +204,12 @@ function checkWinningCondition(currentBoard) {
 //   2 if TIMUN_MAS won
 //   3 if BUTO_IJO won
 function isGameOver(board) {
-<<<<<<< HEAD
     if(checkWinningCondition(board) === 0) {
         return false
     } else if(checkWinningCondition(board) === 1) {
         var turnInfo = document.getElementById("turnInfo");
         turnInfo.innerHTML = messages[1];
     } else if(checkWinningCondition(board) === 2) {
-=======
-    if (checkWinningCondition(board) === 0) {
-        return false
-    } else if (checkWinningCondition(board) === 1) {
-        var turnInfo = document.getElementById("turnInfo");
-        turnInfo.innerHTML = messages[1];
-    } else if (checkWinningCondition(board) === 2) {
->>>>>>> c38c430abe0455f81321f5ae6665fcb670ce1925
         var turnInfo = document.getElementById("turnInfo");
         turnInfo.innerHTML = messages[2];
     } else {
