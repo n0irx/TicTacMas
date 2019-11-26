5020946fdd0189a6901d0e5c83f327efd071d139
Muhammad Nadhirsyah Indra <nadhirsyah@gmail.com>
2019-11-26 19:35:38 +0700

    add button to play music

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -1,49 +1,44 @@
-// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-// console.log(isChrome);
-// if (!isChrome) {
-//     $('#iframeAudio').remove()
-// }
-// else {
-//     $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background 
-// }
-// function setCookie(c_name, value, exdays) {
-//     var exdate = new Date();
-//     exdate.setDate(exdate.getDate() + exdays);
-//     var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-//     document.cookie = c_name + "=" + c_value;
-// }
+// global variable for the player
+var player;
 
-// function getCookie(c_name) {
-//     var i, x, y, ARRcookies = document.cookie.split(";");
-//     for (i = 0; i < ARRcookies.length; i++) {
-//         x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-//         y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-//         x = x.replace(/^\s+|\s+$/g, "");
-//         if (x == c_name) {
-//             return unescape(y);
-//         }
-//     }
-// }
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+	// create the global player from the specific iframe (#video)
+	player = new YT.Player('video', {
+		events: {
+			// call this function when player is ready to use
+			'onReady': onPlayerReady
+		}
+	});
+}
 
-// var song = document.getElementsByTagName('audio')[0];
-// console.log(song);
-// var played = false;
-// var tillPlayed = getCookie('timePlayed');
-// function update() {
-//     if (!played) {
-//         if (tillPlayed) {
-//             song.currentTime = tillPlayed;
-//             song.play();
-//             played = true;
-//         }
-//         else {
-//             song.play();
-//             played = true;
-//         }
-//     }
+function onPlayerReady(event) {
 
-//     else {
-//         setCookie('timePlayed', song.currentTime);
-//     }
-// }
-// setInterval(update, 1000);
+	// bind events
+	var playButton = document.getElementById("play-button");
+	playButton.addEventListener("click", function () {
+		player.playVideo();
+	});
+
+	var pauseButton = document.getElementById("pause-button");
+	pauseButton.addEventListener("click", function () {
+		player.pauseVideo();
+	});
+
+	var stopButton = document.getElementById("stop-button");
+	stopButton.addEventListener("click", function () {
+		player.stopVideo();
+	});
+
+}
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+window.onload = function () {
+	document.getElementById("play-button").click();
+	console.log("click");
+};
